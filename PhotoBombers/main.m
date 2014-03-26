a331fd7ac8588bf3b05df4c5cd59ce7f8da0fc9e
mjvoracek <mjvoracek@gmail.com>
2014-03-26 10:02:49 -0500

    Initial Commit

---
+++ PhotoBombers/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PhotoBombers
+//
+//  Created by Matthew Voracek on 3/26/14.
+//  Copyright (c) 2014 Matthew Voracek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
